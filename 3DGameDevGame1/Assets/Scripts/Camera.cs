3833018accae0b67e7cfc809ee1c92210e739fce
Unknown <lucasremington.gamedesigner@gmail.com>
2018-03-28 23:59:41 -0400

    having problems with local branches

---
+++ 3DGameDevGame1/Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    private Transform look;
+    private Vector3 offset;
+    private Vector3 moveV;
+
+	void Start () {
+        look = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - look.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveV = look.position + offset;
+        moveV.x = 0.0f;
+        moveV.y = Mathf.Clamp(moveV.y, 3, 5);
+
+        transform.position = moveV;
+	}
+}
---
+++ 3DGameDevGame1/Assets/Scripts/Fire.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    public Rigidbody rb;
+    private float speed = 5.0f;
+    public PlayerMovement pm;
+    public GameObject player;
+
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+        pm = player.GetComponent<PlayerMovement>();
+        if (pm.fireval == 0)
+        {
+            rb.velocity = (Vector3.right * speed);
+        }
+        if (pm.fireval == 1)
+        {
+            rb.velocity = (Vector3.left * speed);
+        }
+       
+	}
+	
+	void Update () {
+        Object.Destroy(gameObject,5.0f);
+	}
+}
